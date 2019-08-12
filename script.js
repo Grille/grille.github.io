745dba27536e2fd6751c45166f2235017a8c7066
Paul Hirch <Paul-Jasper@hotmail.de>
2019-08-12 23:55:11 +0200

    update

--- script.js
+++ script.js
@@ -1,64 +1,73 @@
-let projectsHTML = "";
+let projectTables = [];
 
-function main(){
-    let games = new projectTable(html_projectsGam);
-    games.addGH("JS,WebGL","Tank-Game","Top down multiplayer game with tanks")
-    games.addGH("C#,OpenGL","SimCity inspired game")
-    games.addGH("JS,WebGL","AntGame","Isometric RTS Ant Game (engine only)")
-    games.addGH("Java","NodeEngine","Risk like game")
+function main() {
+  let table = null;
 
-    let programs = new projectTable(html_projectsPro);
-    programs.addGH("C#","2D-isometricRenderer","Generate isometric graphics from heightmaps")
-    programs.addGH("C#","StarSim","2D gravitation simulation")
+  table = new ProjectTable("Game related:");
+  table.addGH("JS, WebGL", "Tank-Game", "Top down multiplayer game with tanks");
+  table.addGH("C#, OpenGL", "SimCity inspired game");
+  table.addGH("JS, WebGL", "AntGame", "Isometric RTS Ant Game (engine only)");
+  table.addGH("Java", "NodeEngine", "Risk like game with javafx");
 
-    let librarys = new projectTable(html_projectsLib);
-    librarys.addGH("JS,WebGL","Webgl2D","Simple 2d draw library for WebGL")
-    librarys.addGH("C#,OpenGL","CSGL2D","2d library for OpenGL/OpenTK and C#")
-    librarys.addGH("C#","CsStructParser","Parser for config files used by CityGame")
-    librarys.addGH("C#","ByteStream","Library to write and read binary data")
+  table = new ProjectTable("Editor related:");
+  table.addGH("C#", "2D-isometricRenderer", "Generate isometric graphics from heightmaps");
 
-    let experiments = new projectTable(html_projectsExp);
-    experiments.addGH("JS,Vulkan","NVK-Mandelbrot")
-    experiments.addGH("JS","RetroLine3D","Renderer for 3d wireframe models on canvas 2d")
-    experiments.addGH("JS","ColorPicker","Window for color picking")
+  table = new ProjectTable("Simulation related:");
+  table.addGH("C#", "StarSim", "2D gravitation simulation");
 
-    games.flush();
-    programs.flush();
-    librarys.flush();
-    experiments.flush();
+  table = new ProjectTable("OpenGl/WebGL related:");
+  table.addGH("JS, WebGL", "Webgl2D", "Simple 2d draw library for WebGL");
+  table.addGH("C#, OpenGL", "CSGL2D", "2d library for OpenGL/OpenTK and C#");
+
+  table = new ProjectTable("Parser related:");
+  table.addGH("C#", "CsStructParser", "Parser for config files used by CityGame");
+
+  table = new ProjectTable("Experiments:");
+  table.addGH("JS, Vulkan", "NVK-Mandelbrot","Learning project for Vulkan");
+  table.addGH("JS", "RetroLine3D", "Renderer for 3d wireframe models on canvas 2d");
+
+  table = new ProjectTable("Web:");
+  table.addGH("JS", "ColorPicker", "Window for color picking");
+
+  table = new ProjectTable("Other:");
+  table.addGH("C#", "ByteStream", "Library to write and read binary data");
+
+  flush();
 }
-class projectTable{
-    constructor(html){
-        this.code = "";
-        this.html = html;
+class ProjectTable {
+  constructor(title) {
+    this.title = title;
+    this.code = "";
+    projectTables[projectTables.length] = this;
+  }
+  addGH(tags, repo, name = repo) {
+    let link = "https://github.com/Grille/" + repo;
+    this.add(tags, link, name);
+  }
+  add(tags, link, name = link) {
+    let array = tags.split(',');
+    this.code += `<tr><td><a href=${link}>&#9702 ${name} `;
+    for (let i = 0; i < array.length; i++) {
+      let lname = array[i].trim();
+      let lclass = lname.replace(/[#]/g, "s").replace(/[+]/g, "p").toLowerCase();
+      this.code += `<x-lang class=${lclass}>${lname}</x-lang>`;
     }
-    addGH(tags,repo,name = repo) {
-        let link = "https://github.com/Grille/"+repo;
-        this.add(tags,link,name);
-    }
-    add(tags,link,name = link) {
-        let array = tags.split(',');
-        this.code +=
-        `
-        <tr>
-        <td><a href=${link}>&#9702 ${name}
-        `
-        for (let i = 0; i < array.length; i++) {
-            let lname = array[i];
-            let lclass = lname.replace(/[#]/g, "s").replace(/[+]/g, "p").toLowerCase();
-            this.code += `<x-lang class=${lclass}>${lname}</x-lang>`
-        }
-        this.code +=
-        `
-        </a></td>
-        `
-        //if (demo != null)
-        //    this.code +=`<td><a href=${demo}>(demo)</a></td>`;
-        this.code+=`</tr>`;
-    }
-    flush(){
-        this.html.innerHTML = this.code;
-    }
+    this.code += `</a></td>`;
+    this.code += `</tr>`;
+  }
+}
+function flush() {
+  let html = "";
+  for (let i = 0; i < projectTables.length; i++) {
+    let table = projectTables[i];
+    html += `<h3>${table.title}</h3><table>`;
+    html += table.code;
+    html += `</table>`;
+    if (i < projectTables.length - 1)
+      html += `</br>`;
+  }
+  html_projects.innerHTML = html;
+  html_body.style.visibility = "visible";
 }
 
 window.onload = main;
