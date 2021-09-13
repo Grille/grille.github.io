8fc509506166a3e46ac177c6c8d6d1411126d1d2
Paul <paul-jasper@hotmail.de>
2021-09-13 17:40:15 +0200

    update

--- script.js
+++ script.js
@@ -3,37 +3,36 @@ let projectTables = [];
 function main() {
   let table = null;
 
-  table = new ProjectTable("Game related:");
+  table = new ProjectTable("Game/Engine related:");
   table.add("JS, WebGL", "https://www.youtube.com/channel/UC5bMVzJRThGgpYSkNj3cdwQ", "PokeCraft (Wrote the World Generator)")
   table.addGH("JS, WebGL", "Tank-Game", "Top down multiplayer game with tanks");
-  table.addGH("C#, OpenGL", "CityGame", "SimCity inspired game");
+  table.addGH("C#, WinForm, OpenGL", "CityGame", "SimCity inspired game");
   table.addGH("JS, WebGL", "AntGame", "Isometric RTS Ant Game (engine only)");
-  table.addGH("Java", "NodeEngine", "Risk like game with JavaFX");
-
-  table = new ProjectTable("Editor related:");
-  table.addGH("C#", "2D-isometricRenderer", "Generate isometric graphics from heightmaps");
+  table.addGH("Java, JavaFX", "NodeEngine", "Risk like game with JavaFX");
 
   table = new ProjectTable("Simulation related:");
-  table.addGH("JS,  Vulkan", "Starsim3D","N-body simulation with Vulkan");
-  table.addGH("C#", "StarSim", "Older 2D N-body simulation on CPU");
+  table.addGH("JS, Vulkan", "Starsim3D","N-body simulation with Node-Vulkan");
+  table.addGH("C#, WinForm", "StarSim", "2D N-body simulation");
 
   table = new ProjectTable("GPU/Graphics related:");
-  table.addGH("JS, Vulkan", "SimpleNVK","GPU library with simplified Vulkan functionality");
-  table.addGH("JS, WebGL", "Webgl2D", "Simple 2D drawing library for WebGL");
-  table.addGH("C#, OpenGL", "CSGL2D", "2D library for OpenGL/OpenTK and C#");
+  table.addGH("C#, WinForm", "2D-isometricRenderer", "Generate isometric graphics from heightmaps");
+  table.addGH("JS, Vulkan", "SimpleNVK","Graphic API similar to WebGPU using Node-Vulkan");
+  table.addGH("JS, WebGL", "Webgl2D", "2D drawing library for JS using WebGL");
+  //table.addGH("C#, OpenGL", "CSGL2D", "2D drawing library for C# using OpenGL/OpenTK");
+  table.addGH("JS", "RetroLine3D", "Renderer for 3D wireframe models on canvas 2D");
 
   table = new ProjectTable("Parser related:");
   table.addGH("C#", "CsStructParser", "Parser for config files used by CityGame");
+  table.addGH("C#, WinForm", "ProLernTranspiler", "Transpiler for ProLern learning language & Editor");
 
-  table = new ProjectTable("Experiments:");
+  //table = new ProjectTable("Experiments:");
   //table.addGH("JS, Vulkan", "NVK-Mandelbrot","Realtime mandelbrot renderer in Vulkan");
-  table.addGH("JS", "RetroLine3D", "Renderer for 3D wireframe models on canvas 2D");
+
+  table = new ProjectTable("Utils/Other:");
+  table.addGH("C#", "BinaryView", "Library to write and read binary data");
 
   table = new ProjectTable("Web related:");
   table.addGH("JS", "ColorPicker", "Window for color picking");
-
-  table = new ProjectTable("Other:");
-  table.addGH("C#", "BinaryView", "Library to write and read binary data");
 
   flush();
 }
