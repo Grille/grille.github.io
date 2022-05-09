e05a9e443f91edb43e2f826e529cfc46352e62ec
Paul <paul-jasper@hotmail.de>
2022-05-09 15:26:46 +0200

    update

--- script.js
+++ script.js
@@ -3,36 +3,36 @@ let projectTables = [];
 function main() {
   let table = null;
 
-  table = new ProjectTable("Game/Engine related:");
-  table.add("JS, WebGL", "https://www.youtube.com/channel/UC5bMVzJRThGgpYSkNj3cdwQ", "PokeCraft (Wrote the World Generator)")
+  table = new ProjectTable("Game/Engine:");
   table.addGH("JS, WebGL", "Tank-Game", "Top down multiplayer game with tanks");
   table.addGH("C#, WinForm, OpenGL", "CityGame", "SimCity inspired game");
   table.addGH("JS, WebGL", "AntGame", "Isometric RTS Ant Game (engine only)");
   table.addGH("Java, JavaFX", "NodeEngine", "Risk like game with JavaFX");
 
-  table = new ProjectTable("Simulation related:");
+  table = new ProjectTable("Simulation:");
+  table.addGH("C#, WinForm", "CircuitToy", "CircuitToy");
   table.addGH("JS, Vulkan", "Starsim3D","N-body simulation with Node-Vulkan");
   table.addGH("C#, WinForm", "StarSim", "2D N-body simulation");
 
-  table = new ProjectTable("GPU/Graphics related:");
+  table = new ProjectTable("GPU/Graphics:");
   table.addGH("C#, WinForm", "2D-isometricRenderer", "Generate isometric graphics from heightmaps");
   table.addGH("JS, Vulkan", "SimpleNVK","Graphic API similar to WebGPU using Node-Vulkan");
   table.addGH("JS, WebGL", "Webgl2D", "2D drawing library for JS using WebGL");
   //table.addGH("C#, OpenGL", "CSGL2D", "2D drawing library for C# using OpenGL/OpenTK");
   table.addGH("JS", "RetroLine3D", "Renderer for 3D wireframe models on canvas 2D");
 
-  table = new ProjectTable("Parser related:");
-  table.addGH("C#", "CsStructParser", "Parser for config files used by CityGame");
-  table.addGH("C#, WinForm", "ProLernTranspiler", "Transpiler for ProLern learning language & Editor");
+  table = new ProjectTable("Parser:");
+  table.addGH("C#", "CsStructParser", "Parser for config files, used by CityGame");
+  table.addGH("C#, WinForm", "ProLernTranspiler", "ProLern Transpiler & Editor (german learn language)");
 
-  //table = new ProjectTable("Experiments:");
-  //table.addGH("JS, Vulkan", "NVK-Mandelbrot","Realtime mandelbrot renderer in Vulkan");
-
-  table = new ProjectTable("Utils/Other:");
+  table = new ProjectTable("Utils:");
   table.addGH("C#", "BinaryView", "Library to write and read binary data");
 
-  table = new ProjectTable("Web related:");
+  table = new ProjectTable("Web:");
   table.addGH("JS", "ColorPicker", "Window for color picking");
+
+  table = new ProjectTable("Modding:");
+  table.add("-", "https://www.beamng.com/threads/mbwr-alps-remake-conversion-v0-2.85833/", "(BeamNG) MBWR_Alps Remake")
 
   flush();
 }
